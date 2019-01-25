cae85c96d299391be23e20f1792159fe568cf5ee
Oliver Gill <oliver.gill@comptel.com>
2019-01-25 21:51:20 +0200

    Commit HD project

---
+++ Assets/EnemyMovementTopLeft.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyMovementTopLeft : MonoBehaviour
+{
+    public float speed = 40f;
+    private Rigidbody rb;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector3 movement = new Vector3(0.0f, 0.0f, -1.0f);
+        rb.AddForce(movement * speed);
+    }
+}
